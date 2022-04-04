cd2bbb74a1e5136fc694a3c81a39e5be1d0c8905
18phucduong <phuc.duong@alobridge.com>
2022-04-04 09:58:52 +0700

    fix user_test table structure

--- database/migrations/2022_04_03_175253_create_test_user_table.php
+++ database/migrations/2022_04_03_175253_create_test_user_table.php
@@ -16,10 +16,14 @@ return new class extends Migration
         Schema::create('user_test', function (Blueprint $table) {
             $table->bigInteger('user_id')->unsigned()->nullable();
             $table->bigInteger('test_id')->unsigned()->nullable();
+            $table->bigInteger('question_id')->unsigned()->nullable();
+            $table->bigInteger('answer_id')->unsigned()->nullable();
             $table->float('test_result');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('test_id')->references('id')->on('tests');
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->foreign('answer_id')->references('id')->on('answers');
         });
     }
 
